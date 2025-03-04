fc150535e671c2e4694ac3c7aac39436e9d11e44
iantsai <tsaiiuo1@gmail.com>
2025-03-05 01:19:06 +0800

    Implement q_insert_tail
    Previously, q_inset_tail was an empty function. This commit
    implements the function to properly insert an element at tail
    of queue though list_add_tail function.
    
    Change-Id: I1d8663a399542779087dba1afc031d2e282c21ad

--- queue.c
+++ queue.c
@@ -51,6 +51,18 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    element_t *new_element = malloc(sizeof(element_t));
+    if (!new_element) {
+        return false;
+    }
+
+    new_element->value = strdup(s);
+    if (!new_element->value) {
+        free(new_element);
+        return false;
+    }
+
+    list_add_tail(&(new_element->list), head);
     return true;
 }
 
