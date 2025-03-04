835819ec9ce7856495a6f57d1a481e8ac02dda53
iantsai <tsaiiuo1@gmail.com>
2025-03-05 01:55:38 +0800

    Fix q_remove_tail and q_remove_head
    Previously, both functions did not check the head pointer correctly.
    This commit adds code to verify whether the head pointer is valid
    and whether the queue is empty.
    
    Change-Id: I6a07a96da550bd51218972f1bda2e4958e374a02

--- queue.c
+++ queue.c
@@ -69,6 +69,8 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
+    if (!head || list_empty(head))
+        return NULL;
     element_t *remove_element = list_entry(head->next, element_t, list);
     list_del(head->next);
 
@@ -82,6 +84,8 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
+    if (!head || list_empty(head))
+        return NULL;
     element_t *remove_element = list_entry(head->prev, element_t, list);
     list_del(head->prev);
 
