aa7c8c78097d8063d5021df12f5ec658b1cac6f3
iantsai <tsaiiuo1@gmail.com>
2025-03-07 04:21:37 +0800

    Implement q_descend and q_ascend
    Previously, q_descend and q_ascend was an empty function. This commit
    implements the function to properly achieve function.
    
    Change-Id: I1ffb3645da883aa660db4b0ffb553d8e87476144

--- queue.c
+++ queue.c
@@ -305,16 +305,39 @@ void q_sort(struct list_head *head, bool descend)
  * the right side of it */
 int q_ascend(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    if (!head || list_empty(head))
+        return 0;
+    struct list_head *p = head->next, *pp = p->next;
+    for (; pp != head; pp = pp->next) {
+        element_t *temp = list_entry(pp, element_t, list);
+        if (strcmp(temp->value, list_entry(p, element_t, list)->value) < 0) {
+            list_del(pp);
+            q_release_element(temp);
+        } else {
+            p = pp;
+        }
+    }
+    return q_size(head);
 }
 
 /* Remove every node which has a node with a strictly greater value anywhere to
  * the right side of it */
 int q_descend(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    if (!head || list_empty(head))
+        return 0;
+    struct list_head *p = head->prev, *pp = p->prev;
+    for (; pp != head; pp = pp->prev) {
+        element_t *temp = list_entry(pp, element_t, list);
+        if (strcmp(list_entry(pp, element_t, list)->value,
+                   list_entry(p, element_t, list)->value) < 0) {
+            list_del(pp);
+            q_release_element(temp);
+        } else {
+            p = pp;
+        }
+    }
+    return q_size(head);
 }
 
 /* Merge all the queues into one sorted queue, which is in ascending/descending
